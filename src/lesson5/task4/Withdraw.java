9982204965a180553986c6fb7694fbf384d533b8
andrei <andrey.dvijit@gmail.com>
2020-12-11 17:49:42 +0200

    lesson 5

---
+++ src/lesson5/task4/Withdraw.java
@@ -0,0 +1,28 @@
+package lesson5.task4;
+
+public class Withdraw {
+    public static void main(String[] args) {
+        String[] names = {"Jack", "Ann", "Denis", "Andrey", "Nikolay", "Irina", "John"};
+        int[] balances = {100, 500, 8432, 99, 12000, -54, 0};
+        System.out.println(withdraw(names, balances, "Ann", 10));
+    }
+
+    public static int withdraw(String[] clients, int[] balances, String client, int money) {
+        if (balances[findClientIndexByName(clients, client)] < money) {
+            return -1;
+        }
+        return balances[findClientIndexByName(clients, client)] -= money;
+    }
+
+    public static int findClientIndexByName(String[] clients, String client) {
+        int clientIndex = 0;
+        for (String cl : clients) {
+            if (cl.equals(client)) {
+                break;
+            }
+            clientIndex++;
+        }
+        return clientIndex;
+    }
+
+}
---
+++ src/lesson5/task5/UniqueCount.java
@@ -0,0 +1,27 @@
+package lesson5.task5;
+
+public class UniqueCount {
+
+    public static void main(String[] args) {
+        int[] array = {11, 12, 10, 5, 4, 44, 100, 44, 11, 10, 11, 3};
+        System.out.println(uniqueCount(array));
+    }
+
+    public static int uniqueCount(int[] array) {
+        int result = 0;
+        int countUnique = 0;
+        int count = 0;
+        for (int i = 0; i < array.length; i++) {
+            countUnique++;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] == array[i])
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return result = countUnique - count;
+    }
+
+}
