b57c0f11d7529587984a32bb176d7bff910f6e32
andrei <andrey.dvijit@gmail.com>
2020-12-11 16:47:44 +0200

    lesson 5

---
+++ src/lesson5/ArraysSort.java
@@ -0,0 +1,32 @@
+package lesson5;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class ArraysSort {
+    public static void main(String[] args) {
+        Integer arr[] = {2, 3, 6, 4, 20};
+        sortAscending(arr);
+        sortDescending(arr);
+    }
+
+
+
+    public static void sortAscending(Integer[] array) {
+        //Сортируем массив
+        Arrays.sort(array);
+        //Выводим отсортированный массив на консоль.
+        for (int j : array) {
+            System.out.print(j + "  ");
+        }
+    }
+
+    public static void sortDescending(Integer[] array) {
+        //Сортируем массив
+        Arrays.sort(array, Collections.reverseOrder());
+        //Выводим отсортированный массив на консоль.
+        for (int j : array) {
+            System.out.print(j + "  ");
+        }
+    }
+}
