95a2902053ea6d16744879ece1b787414a29fee9
andreydnb@ukr.net <Cgbhnjdjp4532722>
2020-12-12 12:10:31 +0200

    lesson 6 DbConnector

---
+++ src/lesson6/task3/DbConnector.java
@@ -0,0 +1,13 @@
+package lesson6.task3;
+
+public class DbConnector {
+    public String host;
+    public int port;
+    public String userName;
+    public String password;
+
+    public void connect() {
+        //logic
+    }
+
+}
