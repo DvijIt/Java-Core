d193103b782a7b5a51d982568455015b611f214e
andreydnb@ukr.net <Cgbhnjdjp4532722>
2020-12-12 21:53:40 +0200

    lesson 7 Deal

--- src/lesson7/Demo.java
+++ src/lesson7/Demo.java
@@ -4,16 +4,16 @@ import java.util.Date;
 
 public class Demo {
     public static void main(String[] args) {
-        Deal deal1 = createDeal();
-        Deal deal2 = createDealAndCallMethods();
+        Deal deal1 = createOrder();
+        Deal deal2 = createOrderAndCallMethods();
     }
 
-    public static Deal createDeal() {
+    public static Deal createOrder() {
         Deal deal = new Deal(100, new Date(), false, null, "Dnepr", "Ukraine", "Buy");
         return deal;
     }
 
-    public static Deal createDealAndCallMethods() {
+    public static Deal createOrderAndCallMethods() {
         Deal deal = new Deal(100, new Date(), true, new Date(), "Kiev", "Ukraine", "SomeValue");
         deal.confirmOrder();
         deal.checkPrice();
