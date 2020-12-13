a02195f81963db51831bb8050590a4faf8dfe2b7
andreydnb@ukr.net <Cgbhnjdjp4532722>
2020-12-13 19:00:38 +0200

    lesson 8 Adder

--- src/lesson8/arithmetic/Arithmetic.java
+++ src/lesson8/arithmetic/Arithmetic.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 
 public class Arithmetic {
     static boolean check(int[] array) {
+        if (array == null) {
+            return false;
+        }
         Arrays.sort(array);
         return array[0] + array[array.length - 1] > 100;
     }
--- src/lesson8/arithmetic/Demo.java
+++ src/lesson8/arithmetic/Demo.java
@@ -2,8 +2,18 @@ package lesson8.arithmetic;
 
 public class Demo {
     public static void main(String[] args) {
-        int[] arr = {2, 3, 6, 4, 20};
-        System.out.println(Adder.check(arr));
+        int[] array = {1, 234, 234, 444, -40};
+        System.out.println(Adder.check(array));
         System.out.println(Adder.add(2,6));
+
+        System.out.println(Adder.add(4, 6));
+        System.out.println(Adder.add(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        System.out.println();
+
+        int[] array1 = {1, 66, 35, 100, 55};
+        System.out.println(Arithmetic.check(array1));
+
+        System.out.println(Arithmetic.check(null));
+        System.out.println();
     }
 }
