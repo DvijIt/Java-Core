4810606af0e329c885119b027bf67ec9cd1f50ce
andreydnb@ukr.net <Cgbhnjdjp4532722>
2020-12-13 17:50:55 +0200

    lesson 8

---
+++ src/lesson8/phone/IPhone7.java
@@ -0,0 +1,10 @@
+package lesson8.phone;
+
+public class IPhone7 extends IPhone {
+
+    private boolean isGold;
+
+    public IPhone7(int price, double weight, String countryProduced, boolean fingerPrint) {
+        super(price, weight, countryProduced, fingerPrint);
+    }
+}
