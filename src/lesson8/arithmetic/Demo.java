8999bf961106f55b142908a431806e0316048b06
andreydnb@ukr.net <Cgbhnjdjp4532722>
2020-12-13 19:02:01 +0200

    lesson 8 Adder

--- src/lesson8/arithmetic/Demo.java
+++ src/lesson8/arithmetic/Demo.java
@@ -5,15 +5,5 @@ public class Demo {
         int[] array = {1, 234, 234, 444, -40};
         System.out.println(Adder.check(array));
         System.out.println(Adder.add(2,6));
-
-        System.out.println(Adder.add(4, 6));
-        System.out.println(Adder.add(Integer.MAX_VALUE, Integer.MAX_VALUE));
-        System.out.println();
-
-        int[] array1 = {1, 66, 35, 100, 55};
-        System.out.println(Arithmetic.check(array1));
-
-        System.out.println(Arithmetic.check(null));
-        System.out.println();
     }
 }
