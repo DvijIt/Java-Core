6d87be0500c9baa23409d18e71c0791ea6c14aa1
dvij <andreydnb@ukr.net>
2021-01-10 21:47:58 +0200

    lesson 11 task 1 - FirstInterface

---
+++ src/lesson11/task1/FirstInterface.java
@@ -0,0 +1,8 @@
+package lesson11.task1;
+
+public interface FirstInterface {
+
+    void send();
+
+    String receive();
+}
---
+++ src/lesson11/task2/DbProvider.java
@@ -0,0 +1,10 @@
+package lesson11.task2;
+
+public interface DbProvider {
+
+    void connectToDb();
+
+    void disconnectToDb();
+
+    void encryptData();
+}
