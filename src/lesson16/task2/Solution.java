1d42066b5d6a99a86f9a5fc4a039408dce744b5e
dvij <andreydnb@ukr.net>
2021-02-10 17:33:25 +0200

    lesson 17

---
+++ src/lesson16/task2/Solution.java
@@ -0,0 +1,12 @@
+package lesson16.task2;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int countWords(String input) {
+        return input.trim().split(" ").length;
+    }
+
+}
