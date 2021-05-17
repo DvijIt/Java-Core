1ef9d85836ca7a30018cae4c5d6de61fa592699f
Dvij <andreydnb@ukr.net>
2021-05-17 22:05:57 +0300

    lessons31 hw

--- src/lesson32/ReadFromKeyboard.java
+++ src/lesson32/ReadFromKeyboard.java
@@ -1,7 +1,5 @@
 package lesson32;
 
-import lesson24.exercise.Sys;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -20,37 +18,35 @@ public class ReadFromKeyboard {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter your name");
-
-        if (validateStringInput(scanner.nextLine())) {
-            throw new Exception("Input must have one word");
-        }
-
-        for (char cr : scanner.nextLine().toCharArray()) {
-            if (!validateCharInput(cr)) {
-                throw new Exception("Input must have only letters");
-            }
-        }
-
         System.out.println("Hello, " + scanner.nextLine());
 
         scanner.close();
     }
 
     public static void readKeyboardWithIOStream() throws IOException {
-
         InputStreamReader reader = new InputStreamReader(System.in);
-
         BufferedReader br = new BufferedReader(reader);
 
         System.out.println("Please enter your name");
-        System.out.println("Hello, " + br.readLine() + "!");
+
+        try {
+            validateInput(br.readLine());
+            System.out.println("Hello, " + br.readLine() + "!");
+        } catch (IOException e) {
+            System.err.println(e.getMessage() + "\n");
+            System.out.println("Try again");
+            readKeyboardWithIOStream();
+        }
     }
 
-    public static boolean validateCharInput(Character character) {
-        return Character.isLetter(character);
-    }
-
-    public static boolean validateStringInput(String line) {
-        return line.split(" ").length == 1;
+    public static void validateInput(String input) throws IOException {
+        for (char cr : input.toCharArray()) {
+            if (!Character.isLetter(cr)) {
+                throw new IOException("Input must have only letters");
+            }
+        }
+        if (input.split(" ").length > 1) {
+            throw new IOException("Input must have one word");
+        }
     }
 }
